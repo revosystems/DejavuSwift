6a41f11ec73cf62454d0e925b506f174185780e1
Jordi Puigdellívol <jordi.p@revo.works>
2025-01-17 13:07:31 +0100

    Added adaptiveStack

---
+++ Sources/DejavuSwift/AdaptiveStack.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+import Foundation
+
+public struct AdaptiveStack<Content: View>: View {
+    @Environment(\.horizontalSizeClass) var sizeClass
+    let horizontalAlignment: HorizontalAlignment
+    let verticalAlignment: VerticalAlignment
+    let spacing: CGFloat?
+    let content: () -> Content
+
+    public init(horizontalAlignment: HorizontalAlignment = .center, verticalAlignment: VerticalAlignment = .center, spacing: CGFloat? = nil, @ViewBuilder content: @escaping () -> Content) {
+        self.horizontalAlignment = horizontalAlignment
+        self.verticalAlignment = verticalAlignment
+        self.spacing = spacing
+        self.content = content
+    }
+
+    public var body: some View {
+        Group {
+            if sizeClass == .compact {
+                VStack(alignment: horizontalAlignment, spacing: spacing, content: content)
+            } else {
+                HStack(alignment: verticalAlignment, spacing: spacing, content: content)
+            }
+        }
+    }
+    
+}
+
+
+#Preview {
+    AdaptiveStack {
+        Text("On iPad this will be horizontal stack")
+        Text("However")
+        Text("On iPhone it will be vertical")
+    }
+}
