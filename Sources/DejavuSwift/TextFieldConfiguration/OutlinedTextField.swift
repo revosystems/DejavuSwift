fa17cd66eb98adcc21d5fe935bef55d48e9db4cd
Jordi Puigdellívol <jordi.p@revo.works>
2025-11-07 12:44:37 +0100

    Added couple files

---
+++ Sources/DejavuSwift/TextFieldConfiguration/OutlinedTextField.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+import Foundation
+
+public struct OutlinedTextFieldStyle: TextFieldStyle {
+    
+    @State var icon: Image?
+    
+    public func _body(configuration: TextField<Self._Label>) -> some View {
+        HStack {
+            if icon != nil {
+                icon
+                    .foregroundColor(Color.secondary)
+            }
+            configuration
+        }
+        .padding(6)
+        .overlay {
+            RoundedRectangle(cornerRadius: 8, style: .continuous)
+                .stroke(Color.secondary, lineWidth: 1)
+        }
+    }
+}
