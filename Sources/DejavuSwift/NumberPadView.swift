75e4f1255b2054563f929a277aaae88eb1a33b1c
Jordi Puigdellívol <jordipuigdellivol@Jordis-Mac-mini.local>
2026-03-10 15:51:41 +0100

    Addin min and max to numberpad

--- Sources/DejavuSwift/NumberPadView.swift
+++ Sources/DejavuSwift/NumberPadView.swift
@@ -11,6 +11,9 @@ public struct NumberPadView: View {
     @Binding var value: Double
     let mode: NumberPadMode
     let maxDigits: Int?
+    let minValue:Double?
+    let maxValue:Double?
+    
     private var hasDecimalPoint: Bool { input.contains(".") }
     
     @State private var input: String = "0" {
@@ -28,11 +31,15 @@ public struct NumberPadView: View {
     public init(
         value: Binding<Double>,
         mode: NumberPadMode = .integer,
+        minValue:Double? = nil,
+        maxValue:Double? = nil,
         maxDigits: Int? = nil,
         confirm: ((Double) -> Void)? = nil
     ) {
         self._value = value
         self.mode = mode
+        self.minValue = minValue
+        self.maxValue = maxValue
         self.maxDigits = maxDigits
         self.confirm = confirm
     }
@@ -140,12 +147,25 @@ public struct NumberPadView: View {
         case .decimal:
             if number == "." && !hasDecimalPoint {
                 input += "."
-            } else if number != "." {
+            } else {
                 input += number
+                if input.contains(".") && input.components(separatedBy: ".").last?.count ?? 0 > 2 {
+                    input = String(input.dropLast())
+                }
             }
         }
         
         value = formatValue()
+        
+        if let minValue, value < minValue {
+            value = minValue
+            input = Self.formatInitialValue(value, mode: mode)
+        }
+        
+        if let maxValue, value > maxValue {
+            value = maxValue
+            input = Self.formatInitialValue(value, mode: mode)
+        }
     }
     
     private func removeNumber() {
@@ -201,19 +221,49 @@ public struct NumberPadView: View {
     struct Preview: View {
         var body: some View {
             ScrollView {
-                // Integer mode
-                NumberPadWrapper(mode: .integer, maxDigits: 5)
-                
-                // Price mode
-                NumberPadWrapper(mode: .price)
-                
-                // Decimal mode
-                NumberPadWrapper(mode: .decimal)
-                
-                // With confirm action
-                NumberPadWrapper(mode: .price, confirm: { value in
-                    print("Tip entered: \(value)")
-                })
+                VStack(spacing: 20){
+                    VStack{
+                        Text("Integer mode")
+                        NumberPadWrapper(mode: .integer, maxDigits: 5)
+                    }
+                    .padding()
+                    .cornerRadius(8)
+                    .border(.gray.opacity(0.2))
+                    
+                    VStack{
+                        Text("Price Mode")
+                        NumberPadWrapper(mode: .price)
+                    }
+                    .padding()
+                    .cornerRadius(8)
+                    .border(.gray.opacity(0.2))
+                    
+                    VStack{
+                        Text("Decimal Mode")
+                        NumberPadWrapper(mode: .decimal)
+                    }
+                    .padding()
+                    .cornerRadius(8)
+                    .border(.gray.opacity(0.2))
+                    
+                    VStack{
+                        Text("With min and max values")
+                        NumberPadWrapper(mode: .decimal, minValue:0, maxValue:10)
+                    }
+                    .padding()
+                    .cornerRadius(8)
+                    .border(.gray.opacity(0.2))
+                    
+                    VStack{
+                        Text("With confirm action")
+                        NumberPadWrapper(mode: .price, confirm: { value in
+                            print("Tip entered: \(value)")
+                        })
+                    }
+                    .padding()
+                    .cornerRadius(8)
+                    .border(.gray.opacity(0.2))
+                }
             }
             .frame(maxWidth: 300)
         }
@@ -223,16 +273,20 @@ public struct NumberPadView: View {
         @State var value: Double = 0
         let mode: NumberPadMode
         let maxDigits: Int?
+        let minValue: Double?
+        let maxValue: Double?
         let confirm: ((Double)->Void)?
         
-        init(mode: NumberPadMode = .integer, maxDigits: Int? = nil, confirm: ((Double)->Void)? = nil) {
+        init(mode: NumberPadMode = .integer, minValue:Double? = nil, maxValue:Double? = nil, maxDigits: Int? = nil, confirm: ((Double)->Void)? = nil) {
             self.mode = mode
+            self.minValue = minValue
+            self.maxValue = maxValue
             self.maxDigits = maxDigits
             self.confirm = confirm
         }
         
         var body: some View {
-            NumberPadView(value: $value, mode: mode, maxDigits: maxDigits, confirm: confirm)
+            NumberPadView(value: $value, mode: mode, minValue:minValue, maxValue:maxValue, maxDigits: maxDigits, confirm: confirm)
         }
     }
     return Preview()
